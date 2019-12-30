9206c6b212f01e6f4b85bc96e569f2c04032af73
刘朝伟 <1090744603@qq.com>
2019-12-30 21:15:16 +0800

    first commit

---
+++ src/main/java/cn/justweb/study_demo/pojo/User.java
@@ -0,0 +1,34 @@
+package cn.justweb.study_demo.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+  private long id;
+  private String name;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
---
+++ src/test/java/cn/justweb/study_demo/StudyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.justweb.study_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
