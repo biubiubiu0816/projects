b861028a25674ea1b6b760ef5c6463f46514be63
刘朝伟 <1090744603@qq.com>
2019-12-30 21:39:29 +0800

    second commit

--- src/main/java/cn/justweb/study_demo/config/pageHelperConfig.java
+++ src/main/java/cn/justweb/study_demo/config/pageHelperConfig.java
@@ -15,7 +15,7 @@ public class pageHelperConfig {
         Properties p = new Properties();
         p.setProperty("offsetAsPageNum", "true"); //offsetAsPageNum:设置为true时，会将RowBounds第一个参数offset当成pageNum页码使用.
         p.setProperty("rowBoundsWithCount", "true"); //rowBoundsWithCount:设置为true时，使用RowBounds分页会进行count查询.
-        p.setProperty("reasonable", "true");//reasonable：启用合理化时，如果pageNum<1会查询第一页，如果pageNum>pages会查询最后一页。
+        p.setProperty("reasonable", "true");
         pageHelper.setProperties(p);
         return pageHelper;
     }
